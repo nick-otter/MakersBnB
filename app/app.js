fe41cd834231b0982a0fa6c1fcf2b3e8452ca4d6
Adam Romano <manoadamro.imp@gmail.com>
2017-09-04 16:58:17 +0100

    got tests working

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,3 +46,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+if (!module.parent) {
+  http.createServer(app).listen(process.env.PORT, function(){
+    console.log("Server listening on port " + app.get('port'));
+  });
+}
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('auth', { title: 'Sign-up' });
+  res.render('index', { title: 'Sign-up' });
 });
 
 module.exports = router;
