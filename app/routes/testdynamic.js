ab551b8b4d48732a57e5f535e1440a1077818415
alessiobortone2 <alessiobortone2@gmail.com>
2017-09-05 16:32:08 +0100

    removed old files

--- app/routes/testdynamic.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/:id', function(req, res){
-   res.send('The id you specified is ' + req.params.id);
-});
-
-module.exports = router;
