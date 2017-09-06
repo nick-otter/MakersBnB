82355c96fe971c99505d027d364b4804c2994e54
Adam Romano <manoadamro.imp@gmail.com>
2017-09-06 11:42:37 +0100

    added view listings component with scripts

---
+++ app/public/javascripts/viewListings.js
@@ -0,0 +1,16 @@
+
+function buildListings (listings) {
+  string = '';
+  for (var i = 0; i < listings.length; i++) {
+    item = listings[i]
+    opener = '<tr class="listing">'
+    content = '<h4 class="listing">' + item['name'] + '</h4>' + '<br />' +
+    '<p class="listing">' +
+      item['description'] +
+    '</p>'
+    closer = '</tr>'
+    string += (opener + content + closer)
+  }
+
+  return string;
+};
