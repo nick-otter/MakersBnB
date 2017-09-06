24b1b09d8fb838beff8be4c69e5f3ab13551fb16
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-09-06 16:24:11 +0100

    rendering listings table partial solution

--- app/public/javascripts/viewListings.js
+++ app/public/javascripts/viewListings.js
@@ -1,16 +1,29 @@
 
-function buildListings (listings) {
+function buildListings (listings) { console.log(listings);
   string = '';
-  for (var i = 0; i < listings.length; i++) {
-    item = listings[i]
-    opener = '<tr class="listing">'
-    content = '<h4 class="listing">' + item['name'] + '</h4>' + '<br />' +
-    '<p class="listing">' +
-      item['description'] +
-    '</p>'
-    closer = '</tr>'
-    string += (opener + content + closer)
-  }
-
+listings.forEach(function(item) {
+  opener = '<tr class="listing">'
+  content = '<h4 class="listing">' + item.name + '</h4>' + '<br />' +
+  '<p class="listing">' +
+    item.description +
+  '</p>'
+  closer = '</tr>'
+  string += (opener + content + closer)
+}
   return string;
 };
+
+//
+//   for (var i = 0; i < listings.length; i++) {
+//     item = listings[i]
+//     opener = '<tr class="listing">'
+//     content = '<h4 class="listing">' + item.name + '</h4>' + '<br />' +
+//     '<p class="listing">' +
+//       item.description +
+//     '</p>'
+//     closer = '</tr>'
+//     string += (opener + content + closer)
+//   }
+//
+//   return string;
+// };
